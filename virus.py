66923644c50b29812e656e306c0c37562497e1b7
Ca'Sandra Smith <casresm3@gmail.com>
2024-11-21 11:33:34 -0500

    Added properties to virus constructor

--- virus.py
+++ virus.py
@@ -4,14 +4,18 @@ class Virus(object):
         # Define the attributes of your your virus
         self.name = name
         # TODO Define the other attributes of Virus
-        pass
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
 
 
 # Test this class
 if __name__ == "__main__":
     # Test your virus class by making an instance and confirming 
     # it has the attributes you defined
+    
+    # test 1
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
